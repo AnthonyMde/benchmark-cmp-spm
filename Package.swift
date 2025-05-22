e99cb574c08eeb582ae0086bcfe361aa382e23af
AnthonyMde <anthony.mamode@gmail.com>
2025-05-22 10:34:47 +0200

    update checksum

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
       .binaryTarget(
          name: "shared",
          url: "https://github.com/AnthonyMde/benchmark-cmp-spm/releases/download/v0.1.1/shared.xcframework.zip",
-         checksum:"e456a5fb30e358f970650943aff17e72927e13e74993823bc7fabe8b45e2f3c7")
+         checksum:"8c50b06290fac7dd6597b381e2c58221148d0244bfc8e122e897f180807f2593")
    ]
 )
