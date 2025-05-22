f3745ba16a709481bb93045f9ac90a7864747050
AnthonyMde <anthony.mamode@gmail.com>
2025-05-22 10:27:25 +0200

    update package.swift to v0.1.1

--- Package.swift
+++ Package.swift
@@ -2,17 +2,17 @@
 import PackageDescription
 
 let package = Package(
-   name: "Shared",
+   name: "shared",
    platforms: [
      .iOS(.v14),
    ],
    products: [
-      .library(name: "Shared", targets: ["Shared"])
+      .library(name: "shared", targets: ["shared"])
    ],
    targets: [
       .binaryTarget(
-         name: "Shared",
-         url: "https://github.com/AnthonyMde/benchmark-cmp-spm/releases/download/v0.1.0/Shared.xcframework.zip",
+         name: "shared",
+         url: "https://github.com/AnthonyMde/benchmark-cmp-spm/releases/download/v0.1.1/shared.xcframework.zip",
          checksum:"e456a5fb30e358f970650943aff17e72927e13e74993823bc7fabe8b45e2f3c7")
    ]
 )
