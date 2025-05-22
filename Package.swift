77a73be3d0dfbe8e24bfd65701d67f0e0a8933bb
AnthonyMde <anthony.mamode@gmail.com>
2025-05-22 14:37:06 +0200

    update package.swift to 0.1.2

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "shared",
-         url: "https://github.com/AnthonyMde/benchmark-cmp-spm/releases/download/v0.1.1/shared.xcframework.zip",
+         url: "https://github.com/AnthonyMde/benchmark-cmp-spm/releases/download/v0.1.2/shared.xcframework.zip",
          checksum:"8c50b06290fac7dd6597b381e2c58221148d0244bfc8e122e897f180807f2593")
    ]
 )
